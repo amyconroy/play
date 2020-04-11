2d9823e8196c6a762d0dd20bb00f19688125311a
a-shmyg <45519165+a-shmyg@users.noreply.github.com>
2020-04-11 15:01:15 +0100

    finished slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,18 +1,63 @@
 
-alert(getPostById("image_1"));
-//showPicture(getPostById("image_1"));
+//polyfills for old browsers, remove the frigging copy pasting
 
-function test() {
-  alert("test1!!");
+var current_slide = 0;
+var posts = getPosts("post_image");
+var total_posts = posts.length;
+
+init();
+slider();
+
+function init() {
+  document.getElementById("prev_slide").addEventListener("click", prevSlide);
+  document.getElementById("next_slide").addEventListener("click", nextSlide);
+}
+
+function slider() {
+  hidePosts(posts);
+
+  posts[current_slide].classList.add("visible");
+
+  if (current_slide < (total_posts-1)) {
+    current_slide++;
+  } else {
+    current_slide = 0;
+  }
+
+  setTimeout(slider, 5000);
+}
+
+function nextSlide() {
+
+  if (current_slide < (total_posts-1)) {
+    current_slide++;
+  } else {
+    current_slide = 0;
+  }
+
+  hidePosts(posts);
+  posts[current_slide].classList.add("visible");
+}
+
+function prevSlide() {
+  if (current_slide > 0) {
+    current_slide--;
+  } else {
+    current_slide = (total_posts-1);
+  }
+
+  hidePosts(posts);
+  posts[current_slide].classList.add("visible");
 }
 
 function getPostById(postId) {
-  return document.getElementById(elementId);
-  //get specific post by name
+  return document.getElementById(postId);
+  //get specific post by name, do we need this?
 }
 
 function getPosts(elementClass) {
   //get all posts by element type name
+  return document.getElementsByClassName(elementClass);
 }
 
 function showPicture(element) {
@@ -20,6 +65,9 @@ function showPicture(element) {
   //for specific post add visible class
 }
 
-function hidePictures(allPosts) {
+function hidePosts(allPosts) {
   //for i in all posts, go thru posts and remove visible class
+  for (i = 0; i < allPosts.length; i++) {
+    allPosts[i].classList.remove("visible");
+  }
 }
