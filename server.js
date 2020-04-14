f046703f94024b7276151ce1db63c9b1d1eb6709
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-04-14 21:33:17 +0100

    update hbs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var app = express();
 var router = express.Router();
