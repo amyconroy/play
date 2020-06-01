811f6ddf2b630efb75fda526b8c0493ad11dd429
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-01 16:53:46 +0100

    user db functions

--- site/routes/login_db.js
+++ site/routes/login_db.js
@@ -7,12 +7,74 @@ let db = new sqlite3.Database('Play.db', sqlite3.OPEN_READWRITE, (err) => {
   console.log('Connected to the PLAY database.');
 });
 
+// "CREATE TABLE Category ("+
+  //"categoryId	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE," +
+  //"categoryName	TEXT NOT NULL UNIQUE," +
+  //"categoryDescription	TEXT NOT NULL" +
+  //");")
 
-// here add in hashing / inserting dummy users / etc
+//////////////////////////
+/// CREATE USER TABLE ////
+/////////////////////////
+exports.createUserTable = function(){
+  db.seralize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS User ("+
+	   "userName	TEXT NOT NULL UNIQUE," +
+	   "userEmail	TEXT NOT NULL UNIQUE,"+
+	   "userPassword	TEXT NOT NULL," +
+	   "userSession	TEXT," +
+	   "userId	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE" +
+     ");";
+  });
+}
+
+///////////////////////
+/// CREATE NEW USER ///
+//////////////////////
+
+// CHANGE THE SESSION ID
+/// PARAM: user variable (JSON AS PER BELOW)
+/// JSON:
+// var user = {'email': req.body.email,
+            // 'username': req.body.username,
+            // 'password' : hashedPassword };
+
+exports.newUser = function(newUser){
+  var query = "INSERT INTO User";
+  query += "(userName, userEmail, userPassword, userSession) VALUES (?, ?, ?, ?));";
+    db.seralize(() => {
+      db.run(query, newUser['username'], newUser['email'], newUser['password'], 'one'{
+        if(error){
+          console.log(error);
+        }
+      });
+    });
+}
+
+
+////////////////
+/// GET USER ///
+////////////////
+
+/// CALLBACK : error, user - error to be set NULL if all good, user NULL if bad
+exports.getUserByUserName = function(username, callback){
+  var query = "SELECT * FROM User WHERE userName = ?;";
+  db.seralize(() => {
+    // use each as all returns everything from db, each runs query first
+    db.each(query, username, (err, rows) =>{
+      if(rows){
+        // return error as null as got data back
+        callback(null, rows);
+      } else{
+        callback(error, null);
+      }
+    });
+  });
+}
+
 
 ///// sqlite queries /////
 const user_select_username = db.prepare('SELECT * FROM User WHERE userName = ?');
-const user_create          = db.prepare('INSERT INTO User (userName, userEmail, userPassword, userSession) VALUES (?, ?, ?, ?);');
 const user_login           = db.prepare('REPLACE INTO User (userName, userEmail, userPassword, userSession) VALUES (?, ?, ?, ?);');
 const user_logout          = db.prepare('REPLACE INTO User (userName, userSession) VALUES (?, NULL);');
 const user_session         = db.prepare('SELECT * FROM User WHERE userSession = ?');
@@ -59,29 +121,6 @@ function userLogout(req) {
   });
 }
 
-app.post('/auth', (req, res) => {
-  var username = req.body.username;
-	var password = req.body.password;
-  res.send("request recieved cap'n, with: "+username+" "+password);
-
-  //should be a basic normal select here
-});
-
-app.post('/register', (req, res) => {
-  var username = req.body.register_user;
-	var password = req.body.register_password;
-  var confirm_password = req.body.conf_password;
-  var email = req.body.register_email;
-
-  if (confirm_password === password) { //check password validity
-    if (!validPass(password)) {
-      res.redirect('/login'); //this is hack, not sure how else to deal apart from maybe a clientside callback? or render
-    }
-
-    res.send("request recieved, registering with info: "+username+password+confirm_password+email);
-  } else {
-    res.redirect('/login');
-  }
 
   //run a select on the username, if it exists say we need a diff USERNAME
   //insert new user into our shiny db
