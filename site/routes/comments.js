0c7fb6337de34ce604bf3410ab4e2d35953408df
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-02 13:57:34 +0100

    comments queries added

--- site/routes/comments.js
+++ site/routes/comments.js
@@ -19,33 +19,35 @@ router.post('/new_comment', function(req, res){
 
 /// DELETE COMMENT ////
 router.post('/delete_comment', function(req, res){
-  var commentId = req.commentId, (rows) =>{
-    commentsDB.deleteComment(commentId, (rows))
-    if(rows) =>{ // add in flash for successful delete
-      commentsDB.getTenRecentComments(error) => {
+  var commentId = req.commentId;
+  commentsDB.deleteComment(commentId, (rows) =>{
+    if(rows){ // add in flash for successful delete
+      commentsDB.getTenRecentComments(error => {
         if(error){
           console.log("can't display 10 most recent comments");
         }
         else{
-          console.log("10 most recent comments displayed!");
+          console.log("10 most recent comments displayed");
         }
-      }
+      });
     }
     else{
       console.log("comment has not been sucessfully deleted");
     }
-  }
+  });
 });
 
 router.post('/all_comments', function(req, rest){
-  commentsDB.getAllComments(err, rows) => {
+  commentsDB.getAllComments(err, rows => {
     if(rows){
+      console.log("got all comments");
       // res.render() here
     }
     else{
-      // diff ress render? null results for what comments to s
+      console.log("did not get all comments");
+      // diff ress render?
     }
-  }
-})
+  });
+});
 
 module.exports = router;
