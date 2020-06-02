f6a6601043904ac23e76290017b40c9afdaa1aa7
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-02 01:44:34 +0100

    add comments db file

---
+++ site/routes/comments_db.js
@@ -0,0 +1,8 @@
+///// init database /////
+var sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('Play.db', sqlite3.OPEN_READWRITE, (err) => {
+  if(err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the PLAY database in products.');
+});
