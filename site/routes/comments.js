de5b6ac6d3abdad8f288868542eb6431dee8769b
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-02 02:22:43 +0100

    add functionality for new comment addition

--- site/routes/comments.js
+++ site/routes/comments.js
@@ -2,10 +2,20 @@ var express = require('express');
 var router = express.Router();
 var commentsDB = require('./comments_db.js');
 
-
-
 router.get('/', function(req, res){
     res.render('comments', {layout : 'comments_head'});
 });
+
+
+/// CREATE NEW COMMENT ////
+
+router.post('/new_comment', function(req, res){
+  var newComment = {
+    userId: req.body.userId,
+    timePosted: now(),
+    content: req.body.content
+  }
+  commentsDB.newComment(newComment);
+})
 
 module.exports = router;
