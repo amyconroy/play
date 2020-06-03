9b9c4620162b4fecd62edc12470355b899bdff3d
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-03 22:31:25 +0100

    dgeag yo

--- site/routes/downloads.js
+++ site/routes/downloads.js
@@ -46,11 +46,13 @@ var getAllCategories = function getAllCategories(callback){
 router.get('/:categoryid', function(req, res){
   console.log(req.session.user);
   console.log(req.sessionID);
-
 // downloads by category
   var byCatArr = [];
+  var catId = req.params.categoryid;
+  console.log("YEET category");
+  console.log(catId);
 
-  getAllCategories(function(byCatArr) {
+  getDownloadsByCategory(function(catId, byCatArr) {
     if(byCatArr){
       res.render('downloads', {
         layout: 'download_head',
@@ -60,12 +62,55 @@ router.get('/:categoryid', function(req, res){
   });
 });
 
-var getDownloadsByCategory = function getDownloadsByCategory(callback){
+var getDownloadsByCategory = function getDownloadsByCategory(catId, callback){
   var downloadsArray = [];
 
-  downloadsDB.getProductsByCategory((error, rows) => {
+  downloadsDB.getProductsByCategory(catId, (err, rows) => {
       if (error) {
-        console.log(error);
+        console.log(err);
+      }
+      if(rows){
+        var product = {
+          productCategory: rows.productCategory,
+          productName: rows.name,
+          productDescription: rows.description,
+          productPrice: rows.price,
+          productImage: rows.image,
+          productId: rows.productId
+        };
+        console.log(product);
+        categoriesArray.push(product);
+      }
+      else{
+        console.log("shit from downloads");
+      }
+    });
+  callback(downloadsArray);
+}
+
+router.get('/allDownloads', function(req, res){
+  console.log(req.session.user);
+  console.log(req.sessionID);
+
+// downloads by category
+  var allDownloads = [];
+
+  getAllDownloads(function(allDownloads) {
+    if(allDownloads){
+      res.render('downloads', {
+        layout: 'download_head',
+        downloadsAll: allDownloads
+      });
+    }
+  });
+});
+
+var getAllDownloads = function getAllDownloads(callback){
+  var downloadsArray = [];
+
+  downloadsDB.getAllProducts((err, rows) => {
+      if (err) {
+        console.log(err);
       }
       if(rows){
         var product = {
