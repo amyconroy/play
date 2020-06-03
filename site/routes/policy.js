804e16e209e803716a64bc656302eaa5e18578b0
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-03 14:06:26 +0100

    working policy route

--- site/routes/policy.js
+++ site/routes/policy.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/policy', function(req, res) {
+router.get('/', function(req, res) {
   console.log("getting page");
   res.render('policy', {layout : 'policy_head'});
 });
