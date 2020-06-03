2f996df2f6ab284a6e52e061e3b9b0711964a4f4
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-03 14:21:29 +0100

    add terms and conditions

--- site/routes/terms.js
+++ site/routes/terms.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('policy', {layout : 'policy_head'});
+  res.render('terms', {layout : 'terms_head'});
 });
 
 module.exports = router;
