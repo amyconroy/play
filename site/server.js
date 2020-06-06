e4b71b4683a4a921d1ab570c47b9cb67a52dbe6a
a-shmyg <45519165+a-shmyg@users.noreply.github.com>
2020-06-06 03:24:52 +0100

    css path broken

--- site/server.js
+++ site/server.js
@@ -127,7 +127,7 @@ app.use('/basket', basketRoute);
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/login', express.static(__dirname + '/public')); //for error message rendering
 app.use('/comments', express.static(__dirname + '/public'));
-app.use('/downloads', express.static(__dirname + '/public'));
+app.use('/downloads/', express.static(__dirname + '/public'));
 ///////////////////////////////
 /// FILL DB WITH DUMMY DATA ///
 ///////////////////////////////
