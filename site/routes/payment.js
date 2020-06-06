f31e797d15ad1cbf1f717204e6a02fa26396db16
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-06 21:31:46 +0100

    payment page

--- site/routes/payment.js
+++ site/routes/payment.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  res.render('payment', {
+      layout : 'index_head',
+      userLoggedIn: req.session.user
+  });
+});
 
 module.exports = router;
