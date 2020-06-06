5fd3ad6b8bbcdf1c431629c1864f1824b2280a46
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-06 21:54:35 +0100

    receipt code

--- site/routes/receipt.js
+++ site/routes/receipt.js
@@ -3,62 +3,58 @@ var router = express.Router();
 var basketDB = require('./basket_db.js');
 
 router.get('/', function(req, res) {
-  var basket = req.session.userBasket;
-  var products = [];
-
-  getProducts(basket, function(products) {
-    if(products){
-      res.render('basket', {
-          layout : 'index_head',
-          userLoggedIn: req.session.user,
-          products: products
-      });
-    }
-  });
-});
-
-router.get('/order', function(req, res){
   var newOrder = {
-    userId: 2,
+    userId: req.session.user,
     orderPrice: 19.99,
     orderDate: Date.now()
   }
-  createOrder(newOrder, function(orderId){
+  createOrder(newOrder, function(orderId) => {
     if(orderId){
+      var basket = req.session.userBasket;
+        for(let i = 0; i < basket.length; i++){
       // for each productId, add OrderDetails
-      var orderDetail = {
-        productId: prodId,
-        orderId: orderId
-      }
-      basketDB.addOrderDetails(orderDetail);
+          var newId = products[i].productId;
+          var orderDetail = {
+            productId: newId,
+            orderId: orderId
+          }
+          basketDB.addOrderDetails(orderDetail);
+        }
     }
     else{
       console.log("ERROR cant get orderId");
     }
-  });
-  basketDB.getReceipt(orderId, (err, rows) => {
-    var products = [];
-    var receiptdetails = [];
-    if(rows.length > 0){
-      // remains the same
-      var receipt = {
-        orderid: rows.orderid,
-        orderprice: rows.totalPrice
+    basketDB.getReceipt(orderId, (err, rows) => {
+      var products = [];
+      var receiptdetails = [];
+      if(rows.length > 0){
+        // remains the same
+        var receipt = {
+          orderid: rows.orderid,
+          orderprice: rows.totalPrice
+        }
+        var product = {
+          name: rows.name,
+          price: rows.price,
+          image: rows.image
+        }
+        products.push(product);
       }
-      var product = {
-        name: rows.name,
-        price: rows.price,
-        image: rows.image
+      else{
+        console.log("ERROR can't get receipt");
       }
-      products.push(product);
-    }
-    else{
-      console.log("ERROR can't get receipt");
-    }
-    // ONLY WANT TO DO THIS ONCE !!!
-    receiptdetails.push(receipt);
+      // ONLY WANT TO DO THIS ONCE !!!
+      receiptdetails.push(receipt);
+      if(receiptdetails){
+        res.render('receipt', {
+            layout : 'index_head',
+            receipt: receiptdetails,
+            product: products,
+            userLoggedIn: req.session.user
+        });
+      }
+    });
   });
-  // res.render with receipt, product
 });
 
 var createOrder = function createOrder(newOrder, callback){
