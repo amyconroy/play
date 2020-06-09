935166a928198f4d8322d870c615fd8a711fea52
a-shmyg <45519165+a-shmyg@users.noreply.github.com>
2020-06-09 14:21:07 +0100

    added locaitons

--- site/public/js/demo_submit.js
+++ site/public/js/demo_submit.js
@@ -7,12 +7,32 @@ var childTextNodes;
 
 var locations = ["room", "ssh", "linkedlist", "stacks", "root", "ssh", "room"]; //linear sequence, advance one thru the other
 
+class Location {
+  constructor(locationName, locationNarration) {
+    this.locationName = locationName;
+    this.locationNarration = locationNarration;
+    /*this.locationStory = locationStory;
+    this.completedTasks = completedTasks;*/
+  }
+
+  welcomeNarration() { //onload call this
+    outputResponseToParent(displayText, locationNarration);
+  }
+  //load new location
+  //read location narration
+}
+
 class Controller {
   constructor(playername, inventory, stats, currentLocation) {
+    var locations = [];
     this.playername = playername; //string
     this.inventory = inventory; //array
     this.stats = stats; //json object
     this.currentLocation = currentLocation;
+  }
+
+  spawnLocations() {
+    this.locations.push(new Location("room", "you see a large, messy room. the area is filled with red bull, crisps, and half opened computer science textbooks."));
   }
 
   introductionText() {
@@ -38,8 +58,15 @@ class Controller {
       outputResponseToParent(displayText, "set name to "+newName);
   }
 
-  advanceNarrative(userInput) {
+  loadLocationNarrative(gotoLocation) {
+    switch(gotoLocation) {
+      case "room":
+        break;
 
+      default:
+        outputResponseToParent(displayText, "you can't go there");
+        break;
+    }
   }
 
   gameCommands(userInput) {
@@ -60,8 +87,9 @@ class Controller {
         break;
       case "goto":
         outputResponseToParent(displayText, "going to "+inputWords[1]);
+        loadLocationNarrative(inputWords[1]);
         break;
-      default:
+      default: //cases for a/b/c?
         outputResponseToParent(displayText, "can't understand command. try again.");
         break;
     }
@@ -87,7 +115,9 @@ function inputHandler(e) {
       fadeText(document.getElementById('welcome'));
       clearChildNodes(displayText);
 
+      game.spawnLocations();
       game.introductionText();
+
       firstInput = true;
       userInput.value = "";
 
@@ -143,47 +173,3 @@ function outputResponseToParent(parent, text) {
   newOutputText.appendChild(textnode);
   parent.appendChild(newOutputText);
 }
-
-
-// 1. print welcome intro (set the scene), say just a demo so you can't win
-// 2. must enter the option exactly as they are presented to you
-// throw up disclaimer stating that you can't win! small sample of the game
-// (user has to enter what is in quotes, if not throw an error saying please try again!)
-// hackily offer the first narration with two options
-//  would you like to 1. go to 'entrance' or 2. speak to 'neill'
-//get.name(); // based on name that user enters (case sensitive)
-//get.narration(); // where name.equals() what they entered, get naration (based on the name )
-//get.node(); // return array of nodes, present as options to user ... (these are the names of other places they can go/ people they can speak to)
-
-
-/*{ "graph": [
-    "name" : "room",
-    "description": "your room desc",
-    "node" : ["ssh"]
-  ],
-  [
-    "name" : "ssh",
-    "description" : "ssh portal room desc",
-    "node" : ["main", "room"]
-  ],
-  [
-    "name" : "main",
-    "description" : "main desc",
-    "node" : ["stacks", "root"]
-  ],
-  [
-    "name" : "stacks",
-    "description" : "stacks desc",
-    "node" : ["linkedlist"]
-  ],
-  [
-    "name" : "linkedlist",
-    "description" : "",
-    "node" : ["root"]
-  ],
-  [
-    "name" : "root",
-    "description" : "Main",
-    "node" : ["ssh"]
-  ]
-}*/
