dd557d22f89dc0ee33d9c014878f3c1699482dbe
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-06-09 18:58:20 +0100

    messing around with getting quotes up and running

--- site/public/js/slider.js
+++ site/public/js/slider.js
@@ -6,15 +6,15 @@ var posts = getPosts("post_image");
 var total_posts = posts.length;
 
 var curr_text = 0;
-var randomFacts = ["Did you know: The first commerical U.S cell phone weighed two pounds!",
-"Did you know: Olivia Newton-John's 'Phyiscal' was the 80's most popular song!",
-"Did you know: The original Gameboy had only five games available in the United States!",
-"Did you know: You couldn't play a CD at home until 1982!",
-"Did you know: The first widely-used disposable camera hit the market in 1986!",
-"Did you know: 80's computers typicall had 64KB of memory!",
-"Did you know: MTV was created in 1981, the first ever video played was 'Video Killed the Radio Star'!",
-"Did you know: McDonald's Chicken McNuggets were introduced in 1981!",
-"Did you know: Coca-Cola changed their iconic formula once during the 80s, causing a consumer uproar!"];
+var randomFacts = ["The first commerical U.S cell phone weighed two pounds!",
+"Olivia Newton-John's 'Phyiscal' was the 80's most popular song!",
+"The original Gameboy had only five games available in the United States!",
+"You couldn't play a CD at home until 1982!",
+"The first widely-used disposable camera hit the market in 1986!",
+"80's computers typicall had 64KB of memory!",
+"MTV was created in 1981, the first ever video played was 'Video Killed the Radio Star'!",
+"McDonald's Chicken McNuggets were introduced in 1981!",
+"Coca-Cola changed their iconic formula once during the 80s, causing a consumer uproar!"];
 
 init();
 slider();
